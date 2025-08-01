03be9ae90a9445b80fa09606e63a0ee68b3a551c
Jean Disset <jdisset@mit.edu>
2025-07-31 22:37:17 -0400

    network designs

---
+++ biocomptools/configs/designs/original_python_prototype.py
@@ -0,0 +1,174 @@
+## {{{                      --     net generation     --
+P = "hEF1a"
+T = "L0.T_4560"
+ERNS = ['CasE', 'Csy4', 'PgU']
+
+UORFS = [
+    None,
+    "1w_uORF",
+    "1x_uORF",
+    "2x_uORF",
+    "3x_uORF",
+    "4x_uORF",
+    "5x_uORF",
+    "6x_uORF",
+    "8x_uORF",
+]
+
+
+COLORS = {
+    'x1': 'mKO2',
+    'x2': 'eBFP2',
+    'b': 'mMaroon1',
+    'y': 'mNeonGreen',
+}
+
+u1 = Slot(
+    part=UORFS,
+    ref_id="U1",
+)
+u2 = Slot(
+    part=UORFS,
+    ref_id="U2",
+)
+u3 = Slot(
+    part=UORFS,
+    ref_id="U3",
+)
+
+
+def make_units(tu_name, erns=None):
+    erns = erns or ERNS
+    recs = [f"{ern}_rec" for ern in erns]
+    return [
+        # marker
+        Unit(slots=[P, COLORS[tu_name], T], name=f"{tu_name}_marker"),
+        # a
+        Unit(slots=[P, u1, recs[0], erns[2], T], name=f"{tu_name}_a+"),
+        Unit(slots=[P, erns[0], T], name=f"{tu_name}_a-"),
+        # b
+        Unit(slots=[P, u2, recs[1], erns[2], T], name=f"{tu_name}_b+"),
+        Unit(slots=[P, erns[1], T], name=f"{tu_name}_b-"),
+        # c
+        Unit(slots=[P, u3, recs[2], COLORS['y'], T], name=f"{tu_name}_c+"),
+        Unit(slots=[P, erns[2], T], name=f"{tu_name}_c-"),
+        # direct_out
+        Unit(slots=[P, COLORS['y'], T], name=f"{tu_name}_direct_out"),
+    ]
+
+
+def make_twoandone_network(erns=None):
+    return Network(
+        cotx=[
+            CoTransfection(
+                name="x1",
+                units=make_units("x1", erns=erns),
+            ),
+            CoTransfection(
+                name="x2",
+                units=make_units("x2", erns=erns),
+            ),
+            CoTransfection(
+                name="b",
+                units=make_units("b", erns=erns),
+            ),
+        ],
+        invert_on_build=True,
+    )
+
+
+def make_units_withskip(tu_name, erns=None, direct=True):
+    erns = erns or ERNS
+    recs = [f"{ern}_rec" for ern in erns]
+    units = [
+        # marker
+        Unit(slots=[P, COLORS[tu_name], T], name=f"{tu_name}_marker"),
+        # a
+        Unit(slots=[P, u1, recs[0], erns[1], T], name=f"{tu_name}_a+"),
+        Unit(slots=[P, erns[0], T], name=f"{tu_name}_a-"),
+        # b
+        Unit(slots=[P, u2, recs[1], COLORS['y'], T], name=f"{tu_name}_b+"),
+        Unit(slots=[P, erns[1], T], name=f"{tu_name}_b-"),
+        # c
+        Unit(slots=[P, u3, recs[2], COLORS['y'], T], name=f"{tu_name}_c+"),
+        Unit(slots=[P, erns[2], T], name=f"{tu_name}_c-"),
+        # direct_out
+    ]
+    if direct:
+        units.append(Unit(slots=[P, COLORS['y'], T], name=f"{tu_name}_direct_out"))
+    return units
+
+
+def make_twoandoneskip_network(erns=None):
+    return Network(
+        cotx=[
+            CoTransfection(
+                name="x1",
+                units=make_units_withskip("x1", erns=erns),
+            ),
+            CoTransfection(
+                name="x2",
+                units=make_units_withskip("x2", erns=erns),
+            ),
+            CoTransfection(
+                name="b",
+                units=make_units_withskip("b", erns=erns),
+            ),
+        ],
+        invert_on_build=True,
+    )
+
+
+def make_units_three(tu_name, erns=None, direct=True):
+    erns = erns or ERNS
+    recs = [f"{ern}_rec" for ern in erns]
+    units = [
+        # marker
+        Unit(slots=[P, COLORS[tu_name], T], name=f"{tu_name}_marker"),
+        # a
+        Unit(slots=[P, u1, recs[0], COLORS['y'], T], name=f"{tu_name}_a+"),
+        Unit(slots=[P, erns[0], T], name=f"{tu_name}_a-"),
+        # b
+        Unit(slots=[P, u2, recs[1], COLORS['y'], T], name=f"{tu_name}_b+"),
+        Unit(slots=[P, erns[1], T], name=f"{tu_name}_b-"),
+        # c
+        Unit(slots=[P, u3, recs[2], COLORS['y'], T], name=f"{tu_name}_c+"),
+        Unit(slots=[P, erns[2], T], name=f"{tu_name}_c-"),
+    ]
+    if direct:
+        units.append(Unit(slots=[P, COLORS['y'], T], name=f"{tu_name}_direct_out"))
+    return units
+
+
+def make_three_network(erns=None):
+    return Network(
+        cotx=[
+            CoTransfection(
+                name="x1",
+                units=make_units_three("x1", erns=erns),
+            ),
+            CoTransfection(
+                name="x2",
+                units=make_units_three("x2", erns=erns),
+            ),
+            CoTransfection(
+                name="b",
+                units=make_units_three("b", erns=erns),
+            ),
+        ],
+        invert_on_build=True,
+    )
+
+
+nets = []
+rotations = [ERNS[i:] + ERNS[:i] for i in range(len(ERNS))]
+permutations = list(it.permutations(ERNS))
+nets += [make_twoandone_network(erns=rot) for rot in rotations]
+nets += [make_twoandoneskip_network(erns=per) for per in permutations]
+nets += [make_three_network(erns=ERNS)]
+
+len(nets)
+
+
+##────────────────────────────────────────────────────────────────────────────}}}
+
